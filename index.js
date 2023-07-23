18f6c21fa7f32db2fe8164b242936a81a2edbbe3
Willams Sousa <osdeving@gmail.com>
2023-07-23 10:37:14 -0300

    feat: :sparkles: instala express

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const express = require('express')
+const app = express()
+
+
 const messages = [
     "Minha primeira mensagem",
     "Minha segunda mensagem"
 ]
 
 console.table(messages)
+
+
+app.get('/hello', (req, resp) => resp.send('Hello, World!'))
+
+app.listen('3000')
