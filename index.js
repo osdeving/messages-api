78a62bdd7dcf587acd7679de436f01be1b67b55e
Willams Sousa <osdeving@gmail.com>
2023-07-23 10:49:28 -0300

    refactor: adiciona port e host a partir de process.env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const APP_PORT = process.env.APP_PORT ?? 3000
+const APP_HOST = process.env.APP_HOST ?? 'https://127.0.0.1'
 
 const messages = [
     "Minha primeira mensagem",
@@ -12,4 +14,4 @@ console.table(messages)
 
 app.get('/hello', (req, resp) => resp.send('Hello, World!'))
 
-app.listen('3000')
+app.listen('3000', () => console.info(`App rodando em ${APP_HOST}:${APP_PORT}`))
